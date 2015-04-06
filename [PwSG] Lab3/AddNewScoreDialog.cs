a455dcc693e0fb922db7ec98b9f780d77e7e1cf3
mateusz2813 <mateusz2813@hotmail.com>
2015-04-06 19:55:25 +0200

    Rozwiązany problem czasu gry, jeśli nie było żadnej bomby.

--- [PwSG] Lab3/AddNewScoreDialog.cs
+++ [PwSG] Lab3/AddNewScoreDialog.cs
@@ -34,19 +34,20 @@ namespace _PwSG__Lab3
 
     public partial class AddNewScoreDialog : Form
     {
+        private int _czas;
+
         public AddNewScoreDialog()
         {
             InitializeComponent();
+            _czas = (int)(DateTime.Now - Logika.Czas).TotalSeconds;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             //dodaj nowy wynik do listy
-
             string ksywka = textBox1.Text;
-            int czas = (int)(DateTime.Now - Logika.Czas).TotalSeconds;
             string rozmiar = Logika.Wiersze.ToString() + "x" + Logika.Kolumny.ToString();
-            Wygrana w = new Wygrana(ksywka, czas, rozmiar);
+            Wygrana w = new Wygrana(ksywka, this._czas, rozmiar);
 
             if(Logika.Wyniki==null)
                 Logika.Wyniki=new List<Wygrana>();
